70711ccadff9e8ddf6e940496bc007ed913c1f26
Guest User <Guest@epicodus-15.local>
2018-01-12 11:31:48 -0800

    Add method in js to push criterion into a criterionArray.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,35 @@
+var allCriterionArrays = [];
+//Creates a criterionArray and then shoves it into allCriterionArrays.
+function criterionsToArray(name, experience, platform, application, size, optimization, startDate){
+  var criterionArray = [];
+  criterionArray.push(name);
+  criterionArray.push(experience);
+  criterionArray.push(platform);
+  criterionArray.push(application);
+  criterionArray.push(size);
+  criterionArray.push(optimization);
+  criterionArray.push(startDate);
+};
+
+// Determines when the next class starts for a particular class.
+function nextClassDateComparator(inputDate, classType){
+
+};
+
+
 $(document).ready(function(){
-  $("form#calculate").submit(function(event) {
+
+  $("form#criterion").submit(function(event) {
     event.preventDefault();
-    var num1 = $("#num1").val();
-    var result = ""
-    result = selectorFunction(num1,operator);
-    alert(result);
+    var name = $("#name").val();
+    var experience = $("input:radio[name=exp]:checked").val();
+    var platformPref = $("input:radio[name=platf]:checked").val();
+    var applicationPref = $("input:radio[name=app]:checked").val();
+    var sizePref = $("input:radio[name=size]:checked").val();
+    var optimizationPref = $("input:radio[name=opt]:checked").val();
+    var dateToStart = $("#date").val();
+    criterionsToArray(name,experience,platformPref,applicationPref,sizePref,optimizationPref,dateToStart);
+
   });
-}
+
+});
