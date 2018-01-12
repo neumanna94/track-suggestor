0a92e4f25195b3e2660f6b7364ab721556be2134
Guest User <Guest@epicodus-15.local>
2018-01-12 12:28:38 -0800

    Debug criterionsToArray and determineSum functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 var allCriterionArrays = [];
+var allPrefArray = [];
 //Creates a criterionArray and then shoves it into allCriterionArrays.
 function criterionsToArray(name, experience, platform, application, size, optimization, startDate){
+
   var criterionArray = [];
   criterionArray.push(name);
   criterionArray.push(experience);
@@ -9,48 +11,48 @@ function criterionsToArray(name, experience, platform, application, size, optimi
   criterionArray.push(size);
   criterionArray.push(optimization);
   criterionArray.push(startDate);
-  allCriterionArrays.push(criterionArray);
+  allCriterionArrays.push(criterionArray); //Will be used to see all surveys ever submitted.
   return criterionArray;
 };
 
-function determineTheirClass(inputCriterionArray){
-  var rubySum;
-  var phpSum;
-  var javaSum;
-  var cSum;
+function determineSum(inputCriterionArray){
+  var rubySum=0;
+  var phpSum=0;
+  var javaSum=0;
+  var cSum=0;
   var exp=inputCriterionArray[1];
   var platf=inputCriterionArray[2];
   var app=inputCriterionArray[3];
   var size=inputCriterionArray[4];
   var opt=inputCriterionArray[5];
+  var outputMatrix=[];
 
-  if(exp<=1){
+  if(exp==1){
     rubySum ++;
     phpSum ++;
-  }else{
+  }else if(exp==4){
+    cSum += 2;
+    javaSum +=2;
+  } else {
     rubySum ++;
     phpSum ++;
     javaSum ++;
     cSum ++;
   }
 
-  if(platf===1){
+  if(platf==1){
     rubySum++;
     phpSum++;
-  } else if(platf===2){
+  } else if(platf==2){
     javaSum++;
-  } else if(platf===3){
-    javaSum++;
-    cSum++;
   } else {
-    javaSum++;
     cSum++;
   }
 
-  if(app===1){
+  if(app==1){
     rubySum++;
     phpSum++;
-  } else if(app===2){
+  } else if(app==2){
     rubySum++;
   }else {
     javaSum++;
@@ -58,25 +60,67 @@ function determineTheirClass(inputCriterionArray){
   }
 
   if(size===1){
-    phpSum ++;
-    rubySum ++;
+    rubySum +=2;
   } else {
     phpSum ++;
     javaSum ++;
     cSum ++;
   }
-  if(opt===1){
-    rubySum++;
-  } else if(opt===2){
+  if(opt==1){
+    rubySum+=3;
+    cSum --;
+    javaSum--;
+  } else if(opt==2){
     phpSum ++;
-  } else if(opt===3){
+  } else if(opt==3){
     javaSum++;
     cSum++;
   } else {
     javaSum+=2;
-    cSum +=2;
+    cSum ++;
   }
+  outputMatrix.push(rubySum);
+  outputMatrix.push(phpSum);
+  outputMatrix.push(javaSum);
+  outputMatrix.push(cSum);
+  allPrefArray.push(outputMatrix); //Will use for statistics later on.
+  return outputMatrix;
 }
+
+function insertSort(inputArrayOfSums){
+  var arrLength = inputArrayOfSums.length;
+  var i, k;
+
+  for(var i=1; i <arrLength; i++){
+    currIVal=inputArrayOfSums[i];
+    k=i-1;
+    while(k>=0&&inputArrayOfSums[k]>currIVal){
+      inputArrayOfSums[k+1]=inputArrayOfSums[k];
+      k=k-1;
+    }
+    inputArrayOfSums[k+1]=currIVal;
+  }
+  return inputArrayOfSums;
+}
+
+function determineClass(inputArrayOfSums){
+  sortedInputArrayOfSums = insertSort(inputArrayOfSums); //Don't know how to make a mappings yet.
+  var highestPref=sortedInputArrayOfSums[3];
+  var secondHighestPref=sortedInputArrayOfSums[2];
+  var secondLeastPref=sortedInputArrayOfSums[1];
+  var leastPref=sortedInputArrayOfSums[0];
+  console.log("I'm in determineClass");
+
+  if(inputArrayOfSums[0]===highestPref){
+    return 0;
+  } else if(inputArrayOfSums[1]===highestPref){
+    return 1;
+  } else if(inputArrayOfSums[2]===highestPref){
+    return 2;
+  } else {
+    return 3;
+    }
+  }
 
 
 
@@ -97,7 +141,7 @@ $(document).ready(function(){
     var sizePref = $("input:radio[name=size]:checked").val();
     var optimizationPref = $("input:radio[name=opt]:checked").val();
     var dateToStart = $("#date").val();
-    criterionsToArray(name,experience,platformPref,applicationPref,sizePref,optimizationPref,dateToStart);
+    alert(determineClass(determineSum(criterionsToArray(name,experience,platformPref,applicationPref,sizePref,optimizationPref,dateToStart))));
 
   });
 
