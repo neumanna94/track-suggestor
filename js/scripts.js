3af5e8b722ee547f997550cfb0ecc22f79acf3dd
Guest User <Guest@epicodus-15.local>
2018-01-12 11:46:41 -0800

    Add determineTheirClass method that will be used to find highest sum to calculate preference

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,76 @@ function criterionsToArray(name, experience, platform, application, size, optimi
   criterionArray.push(size);
   criterionArray.push(optimization);
   criterionArray.push(startDate);
+  allCriterionArrays.push(criterionArray);
+  return criterionArray;
 };
+
+function determineTheirClass(inputCriterionArray){
+  var rubySum;
+  var phpSum;
+  var javaSum;
+  var cSum;
+  var exp=inputCriterionArray[1];
+  var platf=inputCriterionArray[2];
+  var app=inputCriterionArray[3];
+  var size=inputCriterionArray[4];
+  var opt=inputCriterionArray[5];
+
+  if(exp<=1){
+    rubySum ++;
+    phpSum ++;
+  }else{
+    rubySum ++;
+    phpSum ++;
+    javaSum ++;
+    cSum ++;
+  }
+
+  if(platf===1){
+    rubySum++;
+    phpSum++;
+  } else if(platf===2){
+    javaSum++;
+  } else if(platf===3){
+    javaSum++;
+    cSum++;
+  } else {
+    javaSum++;
+    cSum++;
+  }
+
+  if(app===1){
+    rubySum++;
+    phpSum++;
+  } else if(app===2){
+    rubySum++;
+  }else {
+    javaSum++;
+    cSum++;
+  }
+
+  if(size===1){
+    phpSum ++;
+    rubySum ++;
+  } else {
+    phpSum ++;
+    javaSum ++;
+    cSum ++;
+  }
+  if(opt===1){
+    rubySum++;
+  } else if(opt===2){
+    phpSum ++;
+  } else if(opt===3){
+    javaSum++;
+    cSum++;
+  } else {
+    javaSum+=2;
+    cSum +=2;
+  }
+}
+
+
 
 // Determines when the next class starts for a particular class.
 function nextClassDateComparator(inputDate, classType){
